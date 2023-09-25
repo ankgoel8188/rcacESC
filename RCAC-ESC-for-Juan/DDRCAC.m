cf5b7a466b90c99039e664d4b5c62197cffec73d
Ankit <50788675+ankgoel8188@users.noreply.github.com>
2023-09-25 18:21:44 -0400

    hardcoded RCAC PID

--- RCAC-ESC-for-Juan/DDRCAC.m
+++ RCAC-ESC-for-Juan/DDRCAC.m
@@ -15,6 +15,8 @@ classdef DDRCAC
         u_buffer;           % Input buffer
         z_buffer;           % Performance variable buffer
         internal_u_buffer;  % Internal input buffer
+
+        g_int;              %Ankit  9/25/23. Integrator state
     end
     
     methods (Access = public)
@@ -25,6 +27,10 @@ classdef DDRCAC
             obj.Nc = Nc;
             obj.lphi = 2*Nc;
             obj.ltheta = 2*Nc*lu*lz;
+
+            obj.lphi    = 3;    %Ankit  9/25/23. Hard coded PID structure
+            obj.ltheta  = 3;    %Ankit  9/25/23. Hard coded PID structure
+
             obj.nu = nu;
             obj.P = P0*eye(obj.ltheta);
             obj.Rbar = blkdiag(eye(lz),Ru*eye(lu));
@@ -33,6 +39,9 @@ classdef DDRCAC
             obj.z_buffer            = zeros(lz,Nc); %[ z(k-1) ...]
             obj.u_buffer    = zeros(((lu+1)*lu),1);
             obj.internal_u_buffer   = zeros(lu*lz,Nc+1); %[ u(k-1) ...]
+
+            obj.g_int   = 0; 
+            %obj.phi_buffer  = zeros(((lu+1)*lu),3);
         end
         function [obj, u_out, theta_out] = oneStep(obj, u_in, z_in, Nf, freeze)
             
@@ -40,32 +49,35 @@ classdef DDRCAC
             obj.internal_u_buffer(:,2:end) = obj.internal_u_buffer(:,1:end-1);
         
             z_in_norm = z_in./(ones(obj.lz,1) + obj.nu.*abs(z_in));
-                  
+            obj.g_int = obj.g_int + z_in;                                       %Ankit  9/25/23. Integrator state
             if (obj.step>obj.Nc)
                 obj.z_buffer(:,1) = z_in_norm;
             end
 
             %Construct PHI with internal, unconstrained u
+            if 0
+                PHI   = zeros(obj.lu,obj.ltheta); %Phi
+                count = 1;
 
-            PHI   = zeros(obj.lu,obj.ltheta); %Phi
-            count = 1;
-
-            for ii = 1:obj.lu
-                count_internal = 1;
-                phi_temp = zeros(1,obj.lphi*obj.lz);
-                for jj = 1:obj.lz
-                    phi_temp(:,count_internal:(count_internal+obj.lphi-1)) = [obj.internal_u_buffer(ii+(jj-1)*obj.lu,2:obj.Nc+1) obj.z_buffer(jj,:)];
-                    obj.internal_u_buffer(ii+(jj-1)*obj.lu,1) = phi_temp(:,count_internal:count_internal+obj.lphi-1)*obj.theta_buffer(count+count_internal-1:count+count_internal+obj.lphi-2 ,1);
-                    phi_temp(:,count_internal:(count_internal+obj.lphi-1)) = [obj.internal_u_buffer(ii+(jj-1)*obj.lu,1:obj.Nc) obj.z_buffer(jj,:)];
-                    count_internal = count_internal + obj.lphi;
+                for ii = 1:obj.lu
+                    count_internal = 1;
+                    phi_temp = zeros(1,obj.lphi*obj.lz);
+                    for jj = 1:obj.lz
+                        phi_temp(:,count_internal:(count_internal+obj.lphi-1)) = [obj.internal_u_buffer(ii+(jj-1)*obj.lu,2:obj.Nc+1) obj.z_buffer(jj,:)];
+                        obj.internal_u_buffer(ii+(jj-1)*obj.lu,1) = phi_temp(:,count_internal:count_internal+obj.lphi-1)*obj.theta_buffer(count+count_internal-1:count+count_internal+obj.lphi-2 ,1);
+                        phi_temp(:,count_internal:(count_internal+obj.lphi-1)) = [obj.internal_u_buffer(ii+(jj-1)*obj.lu,1:obj.Nc) obj.z_buffer(jj,:)];
+                        count_internal = count_internal + obj.lphi;
+                    end
+                    PHI(ii,count:count+obj.lz*obj.lphi-1) = phi_temp;
+                    count = count + obj.lz*obj.lphi;
                 end
-                PHI(ii,count:count+obj.lz*obj.lphi-1) = phi_temp;
-                count = count + obj.lz*obj.lphi;
+            else
+                PHI = [z_in obj.g_int (obj.z_buffer(:,1)-obj.z_buffer(:,2)) ];  %Ankit  9/25/23. Hard coded PID
             end
 
             obj.u_buffer(obj.lu+1:end,:)       = obj.u_buffer(1:end-obj.lu,:);
             obj.u_buffer(1:obj.lu,:)           = u_in;
-        
+            
             obj.phi_buffer(obj.lu+1:end,:)     = obj.phi_buffer(1:end-obj.lu,:);
             obj.phi_buffer(1:obj.lu,:)         = PHI;
             
